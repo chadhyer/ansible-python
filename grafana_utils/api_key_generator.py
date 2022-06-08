5b8de8da773a46c17846c05e7573772873d6383a
Chad <62719020+chadhyer@users.noreply.github.com>
2022-06-08 14:11:52 -0400

    Update api_key_generator.py
    moved exception handling out of functions and into main

--- grafana_utils/api_key_generator.py
+++ grafana_utils/api_key_generator.py
@@ -36,7 +36,6 @@ Arguments:
                     Default: trusted
 """
 
-from getopt import getopt, GetoptError
 from sys import exit, argv
 from pathlib import Path
 import requests
@@ -44,9 +43,7 @@ import json
 
 
 class MissingArgumentError(Exception):
-    """
-    Exception raised for missing arguments
-    """
+    """Exception raised for missing arguments"""
 
     def __init__(self, argument_list:list):
         self.message = f'Missing argument! {argument_list=}!'
@@ -54,9 +51,8 @@ class MissingArgumentError(Exception):
 
 
 class InvalidUsernameOrPasswordError(Exception):
-    """
-    Exception raised for username or password errors when interacting with the grafana api
-    """
+    """Exception raised for username or password errors when interacting with 
+    the grafana api"""
 
     def __init__(self, message:str):
         self.message = message
@@ -64,9 +60,15 @@ class InvalidUsernameOrPasswordError(Exception):
 
 
 class CreateAPITokenError(Exception):
-    """
-    Exception raised for api token generation errors
-    """
+    """Exception raised for api token generation errors"""
+
+    def __init__(self, message:str):
+        self.message = message
+        super().__init__(self.message)
+
+
+class OrganizationError(Exception):
+    """Exception raised for issues relating to grafana organization"""
 
     def __init__(self, message:str):
         self.message = message
@@ -78,12 +80,13 @@ def help_() -> None:
     print(help_text)
 
 
-def check_arguments(org:str, user:str, password:str, host:str, port:int, api_key_file:Path) -> None:
+def check_arguments(name:str, user:str, password:str, 
+                    host:str, port:int, api_key_file:Path) -> None:
     """
-    Returns None if arguments are holding values else raises MissingArgumentError exception
+    Returns None if arguments are holding values else raises 
+        MissingArgumentError exception
 
         Parameters:
-            org_id (int): Id for the organization
             name (str): Name for the new organization
             user (str): User for grafana (typically admin)
             password (str): Password for the grafana user
@@ -94,25 +97,22 @@ def check_arguments(org:str, user:str, password:str, host:str, port:int, api_key
             None: no issues found and arguments are populated
             MissingArgumentError exception if arguments are None
     """
-    argument_list = [ org, user, password, host, host, port, api_key_file ]
+    argument_list = [ name, user, password, host, host, port, api_key_file ]
     index = 0
     for argument in argument_list:
         var = [key for key, value in locals().items() if value == argument]
         if isinstance(argument, type(None)):
-            try: 
-                raise MissingArgumentError(var)
-            except MissingArgumentError(var) as error:
-                raise SystemExit(error)
+            raise MissingArgumentError(var)
 
         index += 1
 
 
-def check_api_key_file(org:str, api_key_file:Path) -> bool:
+def check_api_key_file(name:str, api_key_file:Path) -> bool:
     """
     Returns True if an api_key exists and False if the api_key is missing
 
         Parameters:
-            org (str): Organization name associated with the api key ( note _apikey is appended to name )
+            name (str): Name associated with the api key
             api_key_file (Path): Path leading to the file storing the API key
 
         Returns:
@@ -126,47 +126,41 @@ def check_api_key_file(org:str, api_key_file:Path) -> bool:
             except json.decoder.JSONDecodeError:
                 data = {}
             if "key" in data and "name" in data:
-                if data['name'] == f'{org}_apikey':
+                if data['name'] == f'{name}':
                     return True
     return False
 
 
-def create_api_token(org_id:int, name:str, user:str, password:str, host:str, port:int) -> dict:
+def create_api_token(name:str, user:str, password:str, 
+                     host:str, port:int) -> dict:
     """
     Returns API token generated using the provided org_id
 
         Parameters:
-            org_id (int): Id for the organization
-            name (str): Name for the new organization ( note _apikey is appended to name )
+            name (str): Name for the apikey
             user (str): User for grafana (typically admin)
             password (str): Password for the grafana user
             host (str): Host for the grafana server
             port (int): Port for the grafana server
         Returns:
-            key (str): API key generated for the org_id
-            None: If there was an issue generating org_id
+            key (dict): API key generated with keys, 'id', 'key', and 'name'
+            None: If there was an issue generating API key
     """
 
     json_data = {
-        'name': f'{name}_apikey',
+        'name': f'{name}',
         'role': 'Admin',
     }
 
     access = f'http://{user}:{password}@{host}:{port}'
     url = access + '/api/auth/keys'
 
-    try:
-        response = requests.post(url, json=json_data)
-    except requests.exceptions.RequestException as error:
-        raise SystemExit(error)
+    response = requests.post(url, json=json_data)
 
     data = json.loads(str(response.content, 'utf-8'))
 
-    try:
-        if "message" in data:
-            raise CreateAPITokenError(data['message'])
-    except CreateAPITokenError as error:
-            raise SystemExit(error)
+    if "message" in data:
+        raise CreateAPITokenError(data['message'])
 
     if "key" in data:
         return data
@@ -194,20 +188,14 @@ def post_org(name:str, user:str, password:str, host:str, port:int) -> int:
     access = f'http://{user}:{password}@{host}:{port}'
     url = access + '/api/orgs'
     
-    try:
-        response = requests.post(url, json=json_data)
-    except requests.exceptions.RequestException as error:
-        raise SystemExit(error)
+    response = requests.post(url, json=json_data)
 
     data = json.loads(str(response.content, 'utf-8'))
     
     if data['message'] == 'Organization name taken':
         url = access + '/api/orgs'
 
-        try:
-            response = requests.get(url)
-        except requests.exceptions.RequestException as error:
-            raise SystemExit(error)
+        response = requests.get(url)
 
         data = json.loads(str(response.content, 'utf-8'))
 
@@ -219,15 +207,37 @@ def post_org(name:str, user:str, password:str, host:str, port:int) -> int:
     if data['message'] == 'Organization created':
         return int(data['orgId'])
 
-    try:
-        if data['message'] == 'invalid username or password':
-            raise InvalidUsernameOrPasswordError(data['message'])
-    except InvalidUsernameOrPasswordError as error:
-        raise SystemExit(error)
+    if data['message'] == 'invalid username or password':
+        raise InvalidUsernameOrPasswordError(data['message'])
+
+
+def switch_org(org_id:int, user:str, password:str, host:str, port:int) -> None:
+    """
+    Returns none if successful in switching provided user to organization
+
+        Parameters:
+            org_id (str): Id for the organization
+            user (str): User for grafana (typically admin)
+            password (str): Password for the grafana user
+            host (str): Host for the grafana server
+            port (int): Port for the grafana server
+    """
+
+    access = f'http://{user}:{password}@{host}:{port}'
+    url = access + '/api/user/using/' + str(org_id)
+    
+    response = requests.post(url)
+
+    data = json.loads(str(response.content, 'utf-8'))
+
+    if (not "message" in data or 
+        not data['message'] == 'Active organization changed'):
+
+        raise OrganizationError('Message response missing!')
 
 
 def main(args):
-    GRAFANA_ORG = 'trusted'
+    GRAFANA_NAME = 'trusted'
     GRAFANA_USER = 'admin'
     GRAFANA_PASS = 'admin'
     GRFANA_HOST = 'localhost'
@@ -247,15 +257,18 @@ def main(args):
             GRAFANA_USER = args[1]
         if current_argument in ('-p', '--pass'):
             GRAFANA_PASS = args[1]
-        if current_argument in ('-o', '--org'):
-            GRAFANA_ORG = args[1]
+        if current_argument in ('-n', '--name'):
+            GRAFANA_NAME = args[1]
         if current_argument in ('-f', '--file'):
             API_KEY_FILE = Path(args[1])
         del args[0]
 
+    try: 
+        check_arguments(GRAFANA_NAME, GRAFANA_USER, GRAFANA_PASS, GRFANA_HOST, GRAFANA_PORT, API_KEY_FILE)
+    except MissingArgumentError as error:
+        raise SystemExit(error)
 
-    check_arguments(GRAFANA_ORG, GRAFANA_USER, GRAFANA_PASS, GRFANA_HOST, GRAFANA_PORT, API_KEY_FILE)
-    api_exists = check_api_key_file(GRAFANA_ORG, API_KEY_FILE)
+    api_exists = check_api_key_file(GRAFANA_NAME, API_KEY_FILE)
 
     if api_exists:
         status = {
@@ -264,14 +277,33 @@ def main(args):
         }
         return status
 
-    org_id = post_org(GRAFANA_ORG, GRAFANA_USER, GRAFANA_PASS, GRFANA_HOST, GRAFANA_PORT)
-    api_key = create_api_token(org_id, GRAFANA_ORG, GRAFANA_USER, GRAFANA_PASS, 
-                            GRFANA_HOST, GRAFANA_PORT)
+    try:
+        org_id = post_org(GRAFANA_NAME, GRAFANA_USER, GRAFANA_PASS, GRFANA_HOST, GRAFANA_PORT)
+    except requests.exceptions.RequestException as error:
+        raise SystemExit(error)
+    except requests.exceptions.RequestException as error:
+        raise SystemExit(error)            
+    except InvalidUsernameOrPasswordError as error:
+        raise SystemExit(error)
+
+    try:
+        switch_org(org_id, GRAFANA_USER, GRAFANA_PASS, GRFANA_HOST, GRAFANA_PORT)
+    except requests.exceptions.RequestException as error:
+        raise SystemExit(error)
+    except OrganizationError as error:
+        raise SystemExit(error)
+    
+    try:
+        api_key = create_api_token(GRAFANA_NAME, GRAFANA_USER, GRAFANA_PASS, GRFANA_HOST, GRAFANA_PORT)
+    except requests.exceptions.RequestException as error:
+        raise SystemExit(error)
+    except CreateAPITokenError as error:
+        raise SystemExit(error)
 
     with API_KEY_FILE.open('w') as file:
         json.dump(api_key, file, sort_keys=True, indent=4)
 
-    api_exists = check_api_key_file(GRAFANA_ORG, API_KEY_FILE)
+    api_exists = check_api_key_file(GRAFANA_NAME, API_KEY_FILE)
 
     if api_exists:
         status = {
